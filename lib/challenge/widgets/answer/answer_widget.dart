48bbdee65221c2b179b31ea84dd6e47349500b25
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2021-04-22 12:46:14 -0300

    Aula 5

--- lib/challenge/widgets/answer/answer_widget.dart
+++ lib/challenge/widgets/answer/answer_widget.dart
@@ -1,76 +1,85 @@
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/answer_model.dart';
 import 'package:flutter/material.dart';
 
 class AnswerWidget extends StatelessWidget {
-  final String title;
-  final bool isRight, isSelected;
+  final AnswerModel answer;
+  final bool isSelected;
+  final bool isdisabled;
+  final VoidCallback onTap;
 
-  const AnswerWidget(
-      {Key? key,
-      required this.title,
-      this.isRight = false,
-      this.isSelected = false})
-      : super(key: key);
+  const AnswerWidget({
+    Key? key,
+    required this.answer,
+    required this.onTap,
+    this.isSelected = false, this.isdisabled = false,
+  }) : super(key: key);
 
   Color get _selectedColorRight =>
-      isRight ? AppColors.darkGreen : AppColors.darkRed;
+      answer.isRight ? AppColors.darkGreen : AppColors.darkRed;
 
   Color get _selectedBorderRight =>
-      isRight ? AppColors.lightGreen : AppColors.lightRed;
+      answer.isRight ? AppColors.lightGreen : AppColors.lightRed;
 
   Color get _selectedColorCardRight =>
-      isRight ? AppColors.lightGreen : AppColors.lightRed;
+      answer.isRight ? AppColors.lightGreen : AppColors.lightRed;
 
   Color get _selectedBorderCardRight =>
-      isRight ? AppColors.green : AppColors.red;
+      answer.isRight ? AppColors.green : AppColors.red;
 
   TextStyle get _selectedTextStyleRight =>
-      isRight ? AppTextStyles.bodyDarkGreen : AppTextStyles.bodyDarkRed;
+      answer.isRight ? AppTextStyles.bodyDarkGreen : AppTextStyles.bodyDarkRed;
 
-  IconData get _selectedIconRight => isRight ? Icons.check : Icons.close;
+  IconData get _selectedIconRight => answer.isRight ? Icons.check : Icons.close;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-      child: Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            color: isSelected ? _selectedColorCardRight : AppColors.white,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.fromBorderSide(BorderSide(
-                color:
-                    isSelected ? _selectedBorderCardRight : AppColors.border))),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Text(
-                title,
-                style:
-                    isSelected ? _selectedTextStyleRight : AppTextStyles.body,
-              ),
+      child: IgnorePointer(
+        ignoring: isdisabled,
+        child: GestureDetector(
+          onTap: onTap,
+          child: Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: isSelected ? _selectedColorCardRight : AppColors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.fromBorderSide(BorderSide(
+                    color:
+                        isSelected ? _selectedBorderCardRight : AppColors.border))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    answer.title,
+                    style:
+                        isSelected ? _selectedTextStyleRight : AppTextStyles.body,
+                  ),
+                ),
+                Container(
+                  width: 24,
+                  height: 24,
+                  decoration: BoxDecoration(
+                      color: isSelected ? _selectedColorRight : AppColors.white,
+                      borderRadius: BorderRadius.circular(500),
+                      border: Border.fromBorderSide(BorderSide(
+                          color: isSelected
+                              ? _selectedBorderRight
+                              : AppColors.border))),
+                  child: isSelected
+                      ? Icon(
+                          _selectedIconRight,
+                          color: Colors.white,
+                          size: 16,
+                        )
+                      : null,
+                )
+              ],
             ),
-            Container(
-              width: 24,
-              height: 24,
-              decoration: BoxDecoration(
-                  color: isSelected ? _selectedColorRight : AppColors.white,
-                  borderRadius: BorderRadius.circular(500),
-                  border: Border.fromBorderSide(BorderSide(
-                      color: isSelected
-                          ? _selectedBorderRight
-                          : AppColors.border))),
-              child: isSelected
-                  ? Icon(
-                      _selectedIconRight,
-                      color: Colors.white,
-                      size: 16,
-                    )
-                  : null,
-            )
-          ],
+          ),
         ),
       ),
     );
--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,38 +1,55 @@
 import 'package:DevQuiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/answer_model.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
-  final String title;
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
 
-  QuizWidget({Key? key, required this.title}) : super(key: key);
+  const QuizWidget({
+    Key? key,
+    required this.question,
+    required this.onChange,
+  }) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
+          SizedBox(
+            height: 64,
+          ),
           Text(
-            title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          AnswerWidget(
-              isRight: true,
-              isSelected: true,
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              isSelected: indexSelected == i,
+              isdisabled: indexSelected != -1,
+              answer: answer(i),
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+                Future.delayed(Duration(seconds: 1))
+                    .then((value) => widget.onChange());
+              },
+            ),
         ],
       ),
     );
